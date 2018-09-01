5e7296c8ae1b758e86afae67a7a1eab2100f419e
seanlutz <seanlutz@outlook.com>
2018-09-01 11:14:10 +0000

    pls work bb

--- bashtest.sh
+++ bashtest.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+message=""
+for var in "$@"
+do
+    message=$message\ $var
+done
+echo $message
+
 git config credential.helper store
 git add -A 
-git commit -m $1
+git commit -m "$message"
 git push
