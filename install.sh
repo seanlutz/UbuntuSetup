1ba22a665a955cc4a15cb2165e7741e308bcd228
seanlutz <seanlutz@outlook.com>
2018-09-06 16:33:35 +0000

    spotify

--- install.sh
+++ install.sh
@@ -1,39 +1,60 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install -y wget nano apt-transport-https ca-certificates curl software-properties-common
+# Directories
+cd ~
+mkdir -p installed/customGitConfig/bin projects school stuff
+mv save installed/customGitConfig/bin
+chmod +x installed/customGitConfig/bin/save
+echo PATH=~/installed/customGitConfig/bin/:$PATH >> ~/.bashrc
 
+
+# Random Software
+sudo apt-get update
+sudo apt-get install -y wget nano apt-transport-https ca-certificates curl software-properties-common git
+
+# VS Code
 wget -O install.deb https://go.microsoft.com/fwlink/?LinkID=760868 
 sudo dpkg -i install.deb
 sudo apt-get install -f -y
 rm install.deb
 
+# Python
 wget -O miniconda.sh https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash miniconda.sh -b -p $HOME/miniconda
+bash miniconda.sh -b -p $HOME/installed/miniconda
 rm miniconda.sh
-echo PATH=~/miniconda/bin/:$PATH >> ~/.bashrc
-PATH=~/miniconda/bin/:$PATH
-pip install mypy
+echo PATH=~/installed/miniconda/bin/:$PATH >> ~/.bashrc
+PATH=~/installed/miniconda/bin/:$PATH
+python -m pip install mypy
 conda install -y flask scikit-learn beautifulsoup4 bokeh
 
 
-
+# Docker
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
 sudo apt-get update
 sudo apt-get install -f docker-ce
 
+# Node ;(
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
+# Golang
 wget -O go.tar.gz https://dl.google.com/go/go1.11.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go.tar.gz
 echo PATH=$PATH:/usr/local/go/bin >> ~/.bashrc
 rm go.tar.gz
 
-    
+# Sublime
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
 sudo apt-get install sublime-text
+
+# Spotify
+sudo apt-get install snapd
+snap install spotify
+
+# Android Studio
+
+# Eclipse
